ef454316310393ea6398589023d79d0c7f5b223f
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-02-12 17:57:01 +0100

    Update app.js

--- app.js
+++ app.js
@@ -29,9 +29,10 @@ server.headersTimeout = 120 * 1000;
 app.post("/check-account", (req, res) => {
   const { email } = req.body;
   if (!email) {
-    const bodyError = {"message": "Invalid mandatory input error", "description": "Input body email can not be empty"};
-    console.log(`POST /check-account || Response Status: 400 Response Body: ${bodyError}`);
+    const bodyError = {message: "Invalid mandatory input error", description: "Input body email can not be empty"};
+    console.log(`POST /check-account || Response Status: 400 Response Body: ` + JSON.stringify(bodyError));
+    return res.status(400).json(bodyError);
   }
   
-  res.status(200);
+  return res.status(200);
 });
