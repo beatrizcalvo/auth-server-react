afc7e68e9f6c0122753d5f554d632ab9f5227b7b
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-02-12 13:52:00 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require("express")
 const bcrypt = require("bcrypt")
-var cors = require('cors')
+const jwt = require("jsonwebtoken")
+
+// Initialize DB from file
+var low = require("lowdb");
+var FileSync = require("lowdb/adapters/FileSync");
 
 // Initialize Express app
 const app = express()
 
 // Set up CORS and JSON middlewares
+var cors = require('cors')
 app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -13,4 +18,9 @@ app.use(express.urlencoded({ extended: true }));
 // Basic home route for the API
 app.get("/", (_req, res) => {
     res.send("Auth API.\nPlease use POST /auth & POST /verify for authentication")
+})
+
+app.post('/check-account', (req, res) => {
+    const { email } = req.body
+    console.log("POST /check-account || Email: " + req.body)    
 })
