1018c38fa2ac4a43e6dc9ee69c36afb81f82cd42
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-02-12 16:42:13 +0100

    Update app.js

--- app.js
+++ app.js
@@ -4,11 +4,17 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3001;
 
+// Set up CORS and JSON middlewares
+var cors = require('cors');
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 // Basic home route for the API
 app.get("/", (req, res) => res.type('html').send(html));
 
-const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
-
+// Server configuration
+const server = app.listen(port, () => console.log(`Auth Server app listening on port ${port}!`));
 server.keepAliveTimeout = 120 * 1000;
 server.headersTimeout = 120 * 1000;
 
