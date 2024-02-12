c8cff3927eeaaa97964cded92079530e02ff9e21
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-02-12 16:36:55 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-const express = require("express")
+const express = require("express");
 
 // Initialize Express app
-const app = express()
+const app = express();
 const port = process.env.PORT || 3001;
 
 // Basic home route for the API
