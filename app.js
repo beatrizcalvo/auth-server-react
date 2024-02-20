c03c0f9cd3b1323c4555479bd078f383a429e3c2
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-02-20 16:01:53 +0000

    Create register endpoint

--- app.js
+++ app.js
@@ -1,43 +1,61 @@
-// Initialize database from File
-var low = require("lowdb");
-var FileSync = require("lowdb/adapters/FileSync");
-var adapter = new FileSync("./database.json");
-var db = low(adapter);
+// External dependencies
+require("dotenv").config();
+const express = require("express");
+const bcrypt = require("bcrypt");
+
+// Require database connection
+const dbConnect = require("./db/dbConnect");
+const User = require("./db/userModel");
+
+// Execute database connection
+dbConnect();
 
 // Initialize Express app
-const express = require("express");
 const app = express();
 const port = process.env.PORT || 3001;
 
-// Define a JWT secret key. This should be isolated by using env variables for security
-const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
-const jwtSecretKey = "secretKey";
-
-// Set up CORS and JSON middlewares
-var cors = require('cors');
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
 // Server configuration
-const server = app.listen(port, () => console.log(`Auth Server app listening on port ${port}!`));
+const server = app.listen(port, () =>
+  console.log(`Auth Server app listening on port ${port}!`),
+);
 server.keepAliveTimeout = 120 * 1000;
 server.headersTimeout = 120 * 1000;
 
-// An endpoint to see if there's an existing account for a given email address
-app.post("/check-account", (req, res) => {
-  console.log(`POST /check-account || Request Body: ` + JSON.stringify(req.body));
-  
-  const { email } = req.body;
-  if (!email) {
-    const bodyError = {message: "Invalid mandatory input error", description: "Input body email can not be empty"};
-    console.log(`POST /check-account || Response Status: 400 ## Response Body: ` + JSON.stringify(bodyError));
-    return res.status(400).json(bodyError);
-  }
+// Register new user
+app.post("/register", (request, response) => {
+  // Hash the password
+  bcrypt
+    .hash(request.body.password, 10)
+    .then((hashedPassword) => {
+      // reate a new user instance and collect the data
+      const user = new User({
+        email: request.body.email,
+        password: hashedPassword,
+      });
 
-  const user = db.get("users").value().filter(user => email === user.email);
-  const bodyOK = {userExists: user.length === 1};
-  console.log(`POST /check-account || Response Status: 200 ## Response Body: ` + JSON.stringify(bodyOK)); 
-  return res.status(200).json(bodyOK);
+      // Save the new user
+      user
+        .save()
+        .then((result) => {
+          response.status(201).send({
+            message: "User created successfully",
+            id: result._id,
+          });
+        })
+        .catch((error) => {
+          response.status(500).send({
+            errors: [{ message: "Error creating user", description: error }],
+          });
+        });
+    })
+    .catch((error) => {
+      // Catch error if the password hash isn't successful
+      response.status(500).send({
+        errors: [
+          { message: "Error hashing password", description: error.message },
+        ],
+      });
+    });
 });
+
+module.exports = app;
---
+++ db/userModel.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: [true, "Please provide an Email!"],
+    unique: [true, "Email already exists!"],
+  },
+  password: {
+    type: String,
+    required: [true, "Please provide a password!"],
+  },
+});
+
+module.exports = mongoose.model.Users || mongoose.model("Users", UserSchema);
