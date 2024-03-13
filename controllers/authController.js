5b5118a6445cee836a577db36b238997ef57a5d3
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-03-13 12:52:21 +0100

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -15,8 +15,18 @@ const loginUser = function (request, response) {
   let errorsList = validateMandatory([{key: "email", value: email}, {key: "password", value: password}]);
   if (!!errorsList) {
     let responseBody = { errors: errorsList };
-    console.error("POST /auth/login ## Request Body: " + JSON.stringify(request.body) + " || Response Status: 400 ## Response Body: " + responseBody);
+    console.error("POST /auth/login ## Request Body: " + JSON.stringify(request.body) + " || Response Status: 400 ## Response Body: " + JSON.stringify(responseBody));
+    response.status(400).send(responseBody);
   }
+
+  // Check if email exists
+  userController.findByEmail(email.toLowerCase())
+    .catch(() => {
+      // Catch error if email does not exist
+      let responseBody = { errors: errorMessages.AUTH_API_F_0002() };
+      console.error('POST /login ## Request Body: {"email": "' + email + '" ...} || Response Status: 404 ## Response Body: ' + JSON.stringify(responseBody));
+      response.status(404).send(responseBody);
+    });
   
   response.status(200).send({result: "OK"});
 };
