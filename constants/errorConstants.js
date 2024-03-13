9ee355fd2da1608f8093bcd35ffbd48fefc6d04b
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-03-13 11:56:59 +0100

    Update errorConstants.js

--- constants/errorConstants.js
+++ constants/errorConstants.js
@@ -1,72 +1,14 @@
-const AUTH_API_T_0001 = function (descriptionError) {
-  return {
-    errors: [
-      {
-        code: "AUTH_API-T-0001",
-        level: "error",
-        message: "Error hashing password",
-        description: descriptionError,
-      },
-    ],
-  };
-};
-
-const AUTH_API_T_0002 = function (descriptionError) {
-  return {
-    errors: [
-      {
-        code: "AUTH_API-T-0002",
-        level: "error",
-        message: "Error creating user",
-        description: descriptionError,
-      },
-    ],
-  };
-};
-
-const AUTH_API_F_0001 = function () {
+const AUTH_API_F_0001 = function (field) {
   return {
     errors: [
       {
         code: "AUTH_API-F-0001",
         level: "error",
-        message: "Application login error",
-        description: "Email not found",
+        message: "Mandatory input missing",
+        description: "Input field " + field + " is mandatory" ,
       },
     ],
   };
 };
 
-const AUTH_API_F_0002 = function () {
-  return {
-    errors: [
-      {
-        code: "AUTH_API-F-0002",
-        level: "error",
-        message: "Application login error",
-        description: "Passwords does not match"
-      }
-    ]
-  };
-};
-
-const AUTH_API_F_0003 = function () {
-  return {
-    errors: [
-      {
-        code: "AUTH_API-F-0003",
-        level: "error",
-        message: "Unauthorized",
-        description: "JWT invalid"
-      }
-    ]
-  };
-};
-
-module.exports = { 
-  AUTH_API_T_0001, 
-  AUTH_API_T_0002, 
-  AUTH_API_F_0001,
-  AUTH_API_F_0002,
-  AUTH_API_F_0003
-};
+module.exports = { AUTH_API_F_0001 };
