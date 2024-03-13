f14ed6baebb88575b5e90717873a379034402d50
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-03-13 17:57:49 +0100

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -116,13 +116,4 @@ const loginUser = function (request, response) {
     });
 };
 
-const authenticateToken = function (request, response, next) {
-  // Extract the token from the Authorization header
-  const authHeader = request.headers["authorization"];
-  const token = authHeader && authHeader.split(" ")[1];
-
-  console.log(request.method + " " + request.originalUrl);  
-  next();
-};
-
-module.exports = { loginUser, registerUser, authenticateToken };
+module.exports = { loginUser, registerUser };
