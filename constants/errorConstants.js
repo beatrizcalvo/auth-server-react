d4ddb42b4a6b8fa4cdc200a53d4a61ade92e4de2
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-03-13 12:34:56 +0100

    Update errorConstants.js

--- constants/errorConstants.js
+++ constants/errorConstants.js
@@ -1,11 +1,9 @@
 const AUTH_API_F_0001 = function (field) {
   return {
-    {
       code: "AUTH_API-F-0001",
       level: "error",
       message: "Mandatory input missing",
       description: "Input field " + field + " is mandatory"
-    }
   };
 };
 
