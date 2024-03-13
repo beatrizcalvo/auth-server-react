3ec8787dc6888572a2c0b62a403cd06302c5b615
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-03-13 16:46:27 +0100

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -22,7 +22,15 @@ const registerUser = function (request, response) {
     console.error("POST /auth/register ## Request Body: " + JSON.stringify(request.body) + " || Response Status: 400 ## Response Body: " + JSON.stringify(responseBody));
     return response.status(400).send(responseBody);
   }
-  
+
+  // Hash the password
+  bcrypt
+    .hash(request.body.password, 10)
+    .then()
+    .catch(error => {
+      // Catch error if the password hash isn't successful
+      const responseBody = {};
+    });
   response.status(200).send({result: "OK"});
 };
 
@@ -42,38 +50,14 @@ const loginUser = function (request, response) {
   userController.findByEmail(email.toLowerCase())
     .then((user) => {
       // Compare the password entered and the hashed password found
-      bcrypt
-        .compare(password, user.password)
-        .then(isMatch => {
-          // Check if password matches
-          if (!isMatch) {
-            const responseBody = { errors: errorMessages.AUTH_API_F_0003() };
-            console.error('POST /auth/login ## Request Body: {"email": "' + email + '" ...} || Response Status: 401 ## Response Body: ' + JSON.stringify(responseBody));
-            return response.status(401).send(responseBody);
-          }
+      const isValid = bcrypt.compare(password, user.password);
+      if (!isValid) {
+        const responseBody = { errors: errorMessages.AUTH_API_F_0003() };
+        console.error('POST /auth/login ## Request Body: {"email": "' + email + '" ...} || Response Status: 401 ## Response Body: ' + JSON.stringify(responseBody));
+        return response.status(401).send(responseBody);
+      }
 
-          // Create JWT token
-          const token = jwt.sign(
-            { iss: "react-test-app", sub: user._id },
-            process.env.JWT_SECRET_KEY,
-            { expiresIn: '1h' }
-          );
-
-          // Return success response
-          let responseBody = {
-            access_token: token,
-            token_type: "Bearer",
-            expires_in: "3600"
-          };
-          console.error('POST /auth/login ## Request Body: {"email": "' + email + '" ...} || Response Status: 200 ## Response Body: ' + JSON.stringify(responseBody));
-          return response.status(200).send(responseBody);
-        })
-        .catch(() => {
-          // Catch error if password do not match
-          const responseBody = { errors: errorMessages.AUTH_API_F_0003() };
-          console.error('POST /auth/login ## Request Body: {"email": "' + email + '" ...} || Response Status: 401 ## Response Body: ' + JSON.stringify(responseBody));
-          return response.status(401).send(responseBody);
-        });
+      return response.status(200).send({});
     })
     .catch(() => {
       // Catch error if email does not exist
