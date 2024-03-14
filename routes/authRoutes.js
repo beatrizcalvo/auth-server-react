dc24c5695df460292cd467650530001284820539
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-03-14 16:21:38 +0100

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,6 +1,9 @@
+require("dotenv").config();
 const createHttpError = require('http-errors');
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const errorMessages = require("../constants/errorConstants");
 const validateRequest = require("../middlewares/validateRequest");
@@ -12,8 +15,12 @@ router.post("/login", validateRequest(loginSchema), async (req, res, next) => {
     const email = req.body.email.toLowerCase();
     const password = req.body.password;
 
+    // Check if email exists
     userController.findByEmail(email)
       .then(user => {
+        // Compare the password entered and the hashed password found
+        const isMatch = bcrypt.compare(password, user.password);
+        if (!isMatch) {}
         res.status(200).send({});
       })
       .catch(error => {
