9bfd83d93c2bf0415f31b145e9987bb0b642423e
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-03-14 15:41:47 +0100

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,13 +1,23 @@
 const express = require("express");
 const router = express.Router();
 
+const errorMessages = require("../constants/errorConstants");
 const validateRequest = require("../middlewares/validateRequest");
 const loginSchema = require("../validators/loginValidator");
+const userController = require("../db/controllers/userController");
 
 router.post("/login", validateRequest(loginSchema), async (req, res, next) => {  
   try {
-    throw Error("Prueba error");
-    res.status(200).send({});
+    const email = req.body.email.toLowerCase();
+    const password = req.body.password;
+
+    userController.findByEmail(email)
+      .then(user => {
+        res.status(200).send({});
+      })
+      .catch(error => {
+        
+      });
     
   } catch (error) {
     next(error);
