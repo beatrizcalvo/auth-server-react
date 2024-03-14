1103991a28b1d7e2f2bfda4f77a55065c1e8a28d
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-03-14 12:22:49 +0100

    Update validateRequest.js

--- middlewares/validateRequest.js
+++ middlewares/validateRequest.js
@@ -10,9 +10,18 @@ const validateRequest = (schema) => async function (req, res, next) {
     next();
     
   } catch (error) {
-    if (error.isJoi) next(createHttpError(400, error));
+    if (error.isJoi) next(createHttpError(400, createValiationErrors(error)));
     next(createHttpError(500, error));
   }
+};
+
+const createValidationErrors = function (error) {
+  error.details.map(err => {
+    console.log(err.type);
+    console.log(err.path);
+    console.log(err.message);
+  });
+  return error;
 };
 
 module.exports = validateRequest;
