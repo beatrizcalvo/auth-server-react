e318cd036c2337f4ee2bb65eeecf869fbaf97013
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-03-14 20:12:22 +0100

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,18 @@
+const createHttpError = require('http-errors');
 const express = require("express");
 const router = express.Router();
 
+const userController = require("../db/controllers/userController");
+
 router.get("/me", (req, res, next) => {
-  console.log("Enter /me endpoint");
-  res.status(200).send({});
+  userController.findByIdWithProfile(req.currentUserId)
+    .then((user) => {
+      console.log(user);
+      res.status(200).send({});
+    })
+    .catch(() => {
+      next();
+    });
 });
 
 module.exports = router;
