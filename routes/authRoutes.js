e2703aacff524b7a6d08a6328ac406493ccff793
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-03-14 10:51:13 +0100

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const validateRequest = require("../middlewares/validateRequest");
 const loginSchema = require("../validators/loginValidator");
 
-router.post("/login", validateRequest(loginSchema), async (req, res) => {
+router.post("/login", validateRequest(loginSchema), async (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
   
