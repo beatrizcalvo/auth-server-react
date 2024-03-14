95b3586389d38ed70325793c211823f406816cf6
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-03-14 13:34:21 +0100

    Update errorConstants.js

--- constants/errorConstants.js
+++ constants/errorConstants.js
@@ -16,12 +16,12 @@ const AUTH_API_F_0002 = function (field) {
   };
 };
 
-const AUTH_API_F_0003 = function () {
+const AUTH_API_F_0003 = function (field, min) {
   return {
     code: "AUTH_API-F-0003",
     level: "error",
-    message: "Unauthorized",
-    description: "Passwords does not match"
+    message: "Invalid imput size",
+    description: "Input field " + field + " length must be at least " + min + " characters long"
   };
 };
 
@@ -37,5 +37,6 @@ const AUTH_API_T_0001 = function (descriptionError) {
 module.exports = { 
   AUTH_API_F_0001, 
   AUTH_API_F_0002,
+  AUTH_API_F_0003,
   AUTH_API_T_0001
 };
