e7f87438d54799abd7f374a992bd0acf8b0cb2e6
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-03-14 18:03:47 +0100

    Update autenticateHandler.js

--- middlewares/autenticateHandler.js
+++ middlewares/autenticateHandler.js
@@ -1,8 +1,12 @@
 const createHttpError = require('http-errors');
 
+const errorMessages = require("../constants/errorConstants");
+
 const autenticateHandler = (req, res, next) => {
   // Get the token from the Authorization header and validate
   const authToken = req.headers.authorization;
+  if (!authToken || !authToken.toLowerCase().startsWith("bearer ")) 
+    return next(createHttpError(401, JSON.stringify([errorMessages.AUTH_API_F_0007()])));
 
   
   console.log("Enter autenticateToken middleware");
