1a3790f0d1241abe6857ca22c66cbd8a79a6c4f8
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-03-14 11:28:52 +0100

    Update errorHandler.js

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,6 +1,6 @@
 const errorHandler = (err, req, res, next) => {
   console.log('Executing error handling middleware');
-  res.status(500).json({ error: 'Internal Server Error' });
+  res.status(err.status || 500).json({ error: 'Internal Server Error' });
 };
 
 module.exports = errorHandler;
