958da4d84db0d50b59155892b964b3c722075412
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-03-14 17:59:06 +0100

    Update autenticateHandler.js

--- middlewares/autenticateHandler.js
+++ middlewares/autenticateHandler.js
@@ -1,4 +1,10 @@
-const autenticateHandler = async (req, res, next) => {
+const createHttpError = require('http-errors');
+
+const autenticateHandler = (req, res, next) => {
+  // Get the token from the Authorization header and validate
+  const authToken = req.headers.authorization;
+
+  
   console.log("Enter autenticateToken middleware");
   next();
 };
