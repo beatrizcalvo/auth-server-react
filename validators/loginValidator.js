6d9eb4c19f3899fc37da9d743a3265e37b4766c1
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-03-14 13:27:15 +0100

    Update loginValidator.js

--- validators/loginValidator.js
+++ validators/loginValidator.js
@@ -1,7 +1,7 @@
 const Joi = require('joi');
 
 const loginSchema = Joi.object({
-    email: Joi.string().email().lowercase().required(),
+    email: Joi.string().email().required(),
     password: Joi.string().min(6).required()
 });
 
