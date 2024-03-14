3f53af23710de7de7cd388d7c8022694d08bf580
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-03-14 09:21:35 +0100

    Update and rename auth.js to authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,25 +2,17 @@ const Joi = require("joi");
 const express = require("express");
 const router = express.Router();
 
-router.post("/login", async (request, response) => {
-  const email = request.body.email;
-  const password = request.body.password;
+router.post("/login", async (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
   
   try {
-    const error = schemaLogin.validate(request.body);
-    if (error) {
-      response.status(400).send(error);
-    }
+    res.status(200).send({});
   } catch (error) {
     const responseBody = { errors: [errorMessages.AUTH_API_T_0001(error.message.replaceAll('"', "'"))] };
     console.error('POST /auth/login ## Request Body: {"email": "' + email + '" ...} || Response Status: 500 ## Response Body: ' + JSON.stringify(responseBody));
-    response.status(500).send(responseBody);
+    res.status(500).send(responseBody);
   }
-});
-
-const schemaLogin = Joi.object({
-  email: Joi.string().email().required(), 
-  password: Joi.string().required()
 });
 
 module.exports = router;
