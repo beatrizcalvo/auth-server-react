3e72817460a968109afdad68dbd2a94adf42149f
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-03-14 09:19:17 +0100

    Delete middleware directory

--- middleware/authenticateToken.js
+++
@@ -1,8 +0,0 @@
-exports.authenticateToken = async function (request, response, next) {
-  // Extract the token from the Authorization header
-  const authHeader = request.headers["authorization"];
-  const token = authHeader && authHeader.split(" ")[1];
-
-  console.log(request.method + " " + request.originalUrl);  
-  next();
-};
