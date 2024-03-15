4be29ffb5a3e3ae75943160b14ad9eb5c2cb84fb
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-03-15 08:17:05 +0100

    Update autenticateHandler.js

--- middlewares/autenticateHandler.js
+++ middlewares/autenticateHandler.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const createHttpError = require('http-errors');
-var jwt = require('jsonwebtoken');
+const jwt = require('jsonwebtoken');
 
 const errorMessages = require("../constants/errorConstants");
 const userController = require("../db/controllers/userController");
@@ -17,22 +17,9 @@ const autenticateHandler = (req, res, next) => {
 
   try {
     // Verify the token and check if the user exists. Any error will return code 401
-    jwt.verify(token, process.env.JWT_SECRET_KEY)
-      .then(decodedToken => {
-        // Check if a user with this id exists in the database
-        userController.findById(decodedToken.sub)
-          .then(() => {
-            console.log("OK");
-            req.currentUserId = decodedToken.sub;
-            next();
-          }).catch((error) => {
-            console.log("catch error2: " + error);
-            next(createHttpError(401, JSON.stringify([errorMessages.AUTH_API_F_0007()])));
-          });
-      }). catch((error) => {
-        console.log("catch error2: " + error); 
-        next(createHttpError(401, JSON.stringify([errorMessages.AUTH_API_F_0007()])));
-      });
+    const decodedToken = jwt.verify(token, process.env.JWT_SECRET_KEY);
+    console.log(decodedToken);
+    
   } catch (error) {
     console.log("catch error1: " + error);
     next(createHttpError(401, JSON.stringify([errorMessages.AUTH_API_F_0007()])));
