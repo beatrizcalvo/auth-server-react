fb4ea8887b770059a35ee27aafffba29a390da49
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-03-15 09:53:14 +0100

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,18 +6,21 @@ const errorMessages = require("../constants/errorConstants");
 const userController = require("../db/controllers/userController");
 
 router.get("/me", (req, res, next) => {
-  console.log(req.currentUserId);
   userController.findByIdWithProfile(req.currentUserId)
     .then((result) => {
-      console.log(result);
       const responseBody = {
         id: result._id,
-        firstName: result.firstName,
-        lastName: result.lastName,
-        profile: {}
+        firstName: result.profile.firstName,
+        lastName: result.profile.lastName,
+        fullName: result.profile.firstName + " " + result.profile.lastName,
+        contactPoint: {
+          electronicAddress: {
+            emailAddress: result.email
+          }
+        }
       };
-      console.log(responseBody);
-      res.status(200).send({});
+      console.log("GET /users/me || Response Status: 200 ## Response Body: " + JSON.stringify(responseBody));
+      res.status(200).send(responseBody);
     })
     .catch(() => {
       next(createHttpError(404, JSON.stringify([errorMessages.AUTH_API_F_0008()])));
