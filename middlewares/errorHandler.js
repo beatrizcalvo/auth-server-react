4bc371da331f366fe58366cf3eb67e84166385cb
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-03-15 08:33:45 +0100

    Update errorHandler.js

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -6,8 +6,9 @@ const errorHandler = (err, req, res, next) => {
                         ? { errors: [errorMessages.AUTH_API_T_0001(err.message.replaceAll('"', "'"))] } 
                         : { errors: JSON.parse(err.message) });
 
-  console.error(req.method + " " + req.originalUrl + " ## Request Body: " + JSON.stringify(req.body) 
-                + " || Response Status: " + statusCode + " ## Response Body: " + JSON.stringify(responseBody));
+  console.error(req.method + " " + req.originalUrl + 
+                (req.body ? " ## Request Body: " + JSON.stringify(req.body) : "") + 
+                " || Response Status: " + statusCode + " ## Response Body: " + JSON.stringify(responseBody));
   
   res.status(statusCode).send(responseBody);
 };
