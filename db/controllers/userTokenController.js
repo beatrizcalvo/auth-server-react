17c064e9dc09935c6e7d51acc5117d0eb6781d98
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-03-22 18:35:45 +0100

    Create userTokenController.js

---
+++ db/controllers/userTokenController.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const UserToken = require("../models/userTokenModel");
+
+const updateToken = function (userId, token) {
+  return UserToken.findOneAndUpdate(
+    { userId: userId }, 
+    { token: token }, 
+    upsert: true
+  );
+};
+
+module.exports = { 
+  updateToken
+};
