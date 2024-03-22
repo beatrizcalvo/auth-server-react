0a27402ccce2d5a57c1e7cdd655c467ed85af9a7
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-03-22 18:49:45 +0100

    Update userTokenModel.js

--- db/models/userTokenModel.js
+++ db/models/userTokenModel.js
@@ -5,6 +5,10 @@ const UserTokenSchema = new mongoose.Schema(
     userId: {
       type: mongoose.Schema.Types.ObjectId,
       required: [true, "Please provide a user id!"]
+    },
+    token: {
+      type: String,
+      required: [true, "Please provide a token!"]
     }
   },
   { timestamps: true }
